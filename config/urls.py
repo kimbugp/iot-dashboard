d99cca0ed3d3d4b6c217536cd703662d22d14a56
Kimbugwe Simon Peter <kimbsimon2@gmail.com>
2019-08-22 20:46:47 +0300

    fix flake8

--- config/urls.py
+++ config/urls.py
@@ -13,11 +13,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-import os
-
 from django.contrib import admin
-from django.urls import include, path, re_path
-from django.views.generic.base import TemplateView
+from django.urls import include, path
 from rest_framework.documentation import include_docs_urls
 
 core_schema_view = include_docs_urls(title='IOT DASHBOARD')
